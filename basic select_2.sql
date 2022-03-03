da733d9ddf919d90ac1ef2ceea297bff3a2086ce
khu_jinwooahn <96bluechip@khu.ac.kr>
2022-03-03 10:53:33 +0900

    Create basic select_2.sql

---
+++ basic select_2.sql
@@ -0,0 +1,5 @@
+-- hackerrank
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+select name 
+from city
+where population > 120000 and countrycode = 'USA';
