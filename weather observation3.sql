960d0e888045820b0bbe27d129fce690a3f138b5
khu_jinwooahn <96bluechip@khu.ac.kr>
2022-03-03 11:01:12 +0900

    Create weather observation3.sql

---
+++ weather observation3.sql
@@ -0,0 +1,5 @@
+-- hackerrank
+-- https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+select distinct(city)
+from station
+where id % 2 = 0;
