3c21f6a9a03c32e84b173d4c06a240635af34be4
khu_jinwooahn <96bluechip@khu.ac.kr>
2022-03-24 00:03:55 +0900

    Create Top earnings.sql

---
+++ Top earnings.sql
@@ -0,0 +1,7 @@
+-- hackerrank
+-- https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+select (months * salary) as earnings, count(*)
+from employee
+group by earnings
+order by earnings desc limit 1;
